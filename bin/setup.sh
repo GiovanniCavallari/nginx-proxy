3ae6bade8107da8977def0009dfea49e19fdd7e7
Giovanni Cavallari <giovanni.cavallari@mercadolivre.com>
2022-04-05 16:40:42 -0300

    feat: updated setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+PATH=$1
+
 sudo apt-get install nginx
-sudo rm /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default*
 sudo mkdir /etc/nginx/sites-available/tunnel
-sudo cp dev/nginx-proxy/conf/tunnel.conf /etc/nginx/sites-available/tunnel/
+sudo cp ${PATH}/conf/tunnel.conf /etc/nginx/sites-available/tunnel/
 sudo ln -s /etc/nginx/sites-available/tunnel/tunnel.conf /etc/nginx/sites-enabled/
 sudo nginx -s reload
