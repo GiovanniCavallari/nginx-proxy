fc30d984a3f6c81f3ac44fc0a1eb47a1215486a5
Giovanni Cavallari <giovanni.cavallari@mercadolivre.com>
2022-04-13 11:39:26 -0300

    feat: update Dockerfile

--- bin/run.bash
+++ bin/run.bash
@@ -9,7 +9,7 @@ INTERNAL_HOST=host.docker.internal
 
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]
 then
-    echo "USAGE: docker run --rm -it -p [HOST_PORT]:80 -e TUNNEL_PATH=[TUNNEL_PATH] -e TUNNEL_URL=$INTERNAL_HOST:[TUNNEL_PORT] --name [CONTAINER_NAME] giocavallari/nginx-proxy";
+    echo "USAGE: docker run --rm -it -p [HOST_PORT]:80 -e TUNNEL_PATH=[TUNNEL_PATH] -e TUNNEL_PORT=[TUNNEL_PORT] --name [CONTAINER_NAME] giocavallari/nginx-proxy";
     echo "ERROR: Missing params";
     exit;
 fi
@@ -19,10 +19,10 @@ then
 
     if [[ $OSTYPE == 'darwin'* ]];
     then
-        docker run --rm -it -p $1:80 -e TUNNEL_PATH=$3 -e TUNNEL_URL=$INTERNAL_HOST:$2/ giocavallari/nginx-proxy
+        docker run --rm -it -p $1:80 -e TUNNEL_PATH=$3 -e TUNNEL_PORT=$2/ giocavallari/nginx-proxy
     elif [[ $OSTYPE == 'linux'* ]]; 
     then
-        docker run --rm -it -p $1:80 -e TUNNEL_PATH=$3 -e TUNNEL_URL=$INTERNAL_HOST:$2/ --net=host --add-host=$INTERNAL_HOST:127.0.0.1 giocavallari/nginx-proxy
+        docker run --rm -it -p $1:80 -e TUNNEL_PATH=$3 -e TUNNEL_PORT=$2/ --net=host --add-host=$INTERNAL_HOST:127.0.0.1 giocavallari/nginx-proxy
     else
         exit;
     fi
@@ -31,10 +31,10 @@ else
 
     if [[ $OSTYPE == 'darwin'* ]];
     then
-        docker run --rm -it -p $1:80 -e TUNNEL_PATH=$3 -e TUNNEL_URL=$INTERNAL_HOST:$2/ --name $4 giocavallari/nginx-proxy
+        docker run --rm -it -p $1:80 -e TUNNEL_PATH=$3 -e TUNNEL_PORT=$2/ --name $4 giocavallari/nginx-proxy
     elif [[ $OSTYPE == 'linux'* ]]; 
     then
-        docker run --rm -it -p $1:80 -e TUNNEL_PATH=$3 -e TUNNEL_URL=$INTERNAL_HOST:$2/ --net=host --add-host=$INTERNAL_HOST:127.0.0.1 --name $4 giocavallari/nginx-proxy
+        docker run --rm -it -p $1:80 -e TUNNEL_PATH=$3 -e TUNNEL_PORT=$2/ --net=host --add-host=$INTERNAL_HOST:127.0.0.1 --name $4 giocavallari/nginx-proxy
     else
         exit;
     fi
